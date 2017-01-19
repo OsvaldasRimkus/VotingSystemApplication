50e79678327d96e72542b99dab3356b585e47e17
andrius444 <andrius.sareika@gmail.com>
2017-01-19 15:43:06 +0200

    Initial commit

---
+++ src/main/java/voting_system/Application.java
@@ -0,0 +1,17 @@
+package voting_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+/**
+ * Created by andrius on 1/19/17.
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
